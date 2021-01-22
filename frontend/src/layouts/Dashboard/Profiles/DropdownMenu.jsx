9bb2c5ff87954fbfb1868ea7f172ed5b943885b4
BadrKilou <Bobbyevin07@gmail.com>
2021-01-22 22:13:50 +0100

    solved dropdown

--- frontend/src/layouts/Dashboard/Profiles/DropdownMenu.jsx
+++ frontend/src/layouts/Dashboard/Profiles/DropdownMenu.jsx
@@ -1,50 +1,43 @@
-import React, { useRef } from 'react'
-import { Link } from 'react-router-dom';
-import { useDetectOutsideClick } from '../../../assets/outsideClick/UseOutsideClick'
-import './styles/DropDown.scss';
-
+import React, { useRef } from "react";
+import { Link } from "react-router-dom";
+import { useDetectOutsideClick } from "../../../assets/outsideClick/UseOutsideClick";
+import "./styles/DropDown.scss";
 
 const DropdownMenu = (props) => {
-   
-
-    const dropdownRef = useRef(null);
+  const dropdownRef = useRef(null);
   const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
   const onClick = () => setIsActive(!isActive);
-    return (
-        <div>
-             <div className="menu-container">
-        <button onMouseEnter={onClick} className="menu-trigger">
-          
-          {props.avatar ?
-          <img
-            src={props.avatar}
-     
-          /> : 
-          <span>{props.user !== null && props.user.name.toUpperCase()}</span>
-          }
+  return (
+    <div>
+      <div className="menu-container">
+        <button onClick={onClick} className="menu-trigger">
+          {props.avatar ? (
+            <img src={props.avatar} />
+          ) : (
+            <span>{props.user !== null && props.user.name.toUpperCase()}</span>
+          )}
         </button>
-        <div className='dropdown'
-          onMouseLeave={onClick}
+        <div
+          className="dropdown"
+          onClick={onClick}
           ref={dropdownRef}
           className={`dropdown-menu ${isActive ? "active" : "inactive"}`}
         >
           <ul>
             <li>
-            <Link to='notes'>{props.notes}</Link>
+              <Link to="notes">{props.notes}</Link>
             </li>
             <li>
-            <Link to='profile'>{props.profile}</Link>
+              <Link to="profile">{props.profile}</Link>
             </li>
             <li>
-            <Link onClick={props.logout}>{props.disconnect}</Link>
+              <Link onClick={props.logout}>{props.disconnect}</Link>
             </li>
           </ul>
         </div>
       </div>
-        </div>
-    )
-}
+    </div>
+  );
+};
 
-
-
-export default  DropdownMenu
+export default DropdownMenu;
