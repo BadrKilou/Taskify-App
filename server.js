d81d9e5dc963a9e57ec8fcbe82d6b0ab508fd7bc
BadrKilou <Bobbyevin07@gmail.com>
2021-01-22 20:59:19 +0100

    better off

--- server.js
+++ server.js
@@ -28,11 +28,15 @@ app.use("/api/file", require("./middleware/cloudinary/cloudinary"));
 app.use("/api/multer", require("./middleware/multer/multerConfig"));
 app.use("/api", require("./routers/resetPassword"));
 
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API RUNNING SUCCESSFULLY...");
   });
 }
 
